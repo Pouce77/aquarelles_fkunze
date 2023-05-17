a4ba851a3c1e217d7cc4ec13f68e6ae1c17ece26
Julien Kunze <julienkunze0@gmail.com>
2023-05-17 13:18:20 +0200

    changing linkcrudcontroller

--- src/Controller/Admin/LinkCrudController.php
+++ src/Controller/Admin/LinkCrudController.php
@@ -3,7 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Link;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class LinkCrudController extends AbstractCrudController
 {
@@ -12,14 +18,25 @@ class LinkCrudController extends AbstractCrudController
         return Link::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->hideOnForm(),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            TextareaField::new('content'),
+            TextField::new('url')
         ];
     }
-    */
+    
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+
+        ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
+            return $action->setIcon('fa fa-add')->setLabel("Ajouter un lien");
+        })
+        ;
+    }
 }
